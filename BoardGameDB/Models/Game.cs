cc4759881afd1e02f674b33a4b9aec9d6b410b71
michaelmiles1 <michael.gage.miles@gmail.com>
2019-10-02 00:59:40 -0700

    Completed Board Game database feature. Beginning to add forum

--- BoardGameDB/Models/Game.cs
+++ BoardGameDB/Models/Game.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BoardGameDB.Models
 {
     public class Game
     {
         public int Id { get; set; }
         public String Title { get; set; }
-        public String ImageName { get; set; }
+        public String ImageURL { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
     }
 }
