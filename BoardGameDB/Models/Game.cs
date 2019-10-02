8c484e3c1a316b5237e54e7be885185bfc5e93d9
michaelmiles1 <michael.gage.miles@gmail.com>
2019-10-01 22:48:19 -0700

    Initial Commit

---
+++ BoardGameDB/Models/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BoardGameDB.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String ImageName { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
